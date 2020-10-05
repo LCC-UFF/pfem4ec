5179fb90f6cf7813e677b36bec1de002477482db
Victor <victorsapucaia@id.uff.br>
2020-10-05 16:19:02 -0300

    Speed up code

--- src/CEP.jl
+++ src/CEP.jl
@@ -10,7 +10,7 @@ using SparseArrays
 
 # -----------------------------------------------------
 # Read NF file
-function readNF(filename)
+function readNF(filename::String)
 
 println("----")
 println("Read NF")
@@ -19,8 +19,8 @@ fileID = open(filename)
 
 # initialize
 m_tol = 0;
-m_materials_I = zeros(UInt64,256,1);
-m_materials_F = zeros(Float64,256,1);
+m_materials_I = zeros(UInt64,256);
+m_materials_F = zeros(Float64,256);
 m_numMat = 0;
 
 while !eof(fileID)
@@ -53,14 +53,14 @@ end
 
 # -----------------------------------------------------
 # Read JSON file
-function readJSON(filename)
+function readJSON(filename::String)
 
 println("----")
 println("Read JSON")
 
 # Initialize
-m_materials_I = zeros(UInt16,256,1);
-m_materials_F = zeros(Float64,256,1);
+m_materials_I = zeros(UInt16,256);
+m_materials_F = zeros(Float64,256);
 
 data = Dict();
 open(filename,"r") do f
@@ -70,48 +70,70 @@ end
 m_tol = data["TOL"];
 m_nx = data["W"];
 m_ny = data["H"];
+m_solver = data["SOLVER"];
 mat = data["MATERIALS"];
 m_numMat = data["NMAT"];
-
 for i=1:m_numMat
     aux = mat[i];
     m_materials_I[convert(UInt8,aux[1])+1] = i;
     m_materials_F[convert(UInt8,aux[1])+1] = convert(Float64, aux[2]);
 end
 
-return m_tol, m_materials_I, m_materials_F, m_numMat, m_nx, m_ny
+return m_tol, m_materials_I, m_materials_F, m_numMat, m_nx, m_ny, m_solver
+end
+# -----------------------------------------------------
+
+# -----------------------------------------------------
+# Read RAW file
+function readRAW(filename::String, m_nx::Int, m_ny::Int)
+
+println("----")
+println("Read RAW")
+
+io = open(filename, "r");
+bin_array = read(io);
+
+m_nref = 1;
+m_matID = reshape(reinterpret(UInt8, bin_array),:);
+m_matID = reshape(m_matID,m_nx,m_ny);
+m_matID = convert(Array{UInt64},kron(m_matID,ones(m_nref,m_nref))).+1;
+m_matID = m_matID[:];
+m_nx = m_nx*m_nref; 
+m_ny = m_nx;
+
+return m_matID, m_nx, m_ny
 end
 # -----------------------------------------------------
 
 # -----------------------------------------------------
 # Element Q4 Conductivity - FEM
-function Q4ElementConductivity(elems_prop)
+function Q4ElementConductivity(elems_prop::Float64)
 
     # Element
-    x = [0;1;1;0]
-    y = [0;0;1;1]
+    x = [0.;1.;1.;0.];
+    y = [0.;0.;1.;1.];
 
     # Constitutive
-    c = elems_prop
-    Ident = Matrix{Float64}(I, 2, 2)
-    C = c*Ident
+    c = elems_prop;
+    Ident = Matrix{Float64}(I, 2, 2);
+    C = c*Ident;
 
     # Gauss Points
-    PG = [-1/sqrt(3) 1/sqrt(3)]
-    w = [1 1]
+    PG = [-1/sqrt(3) 1/sqrt(3)];
+    w = [1 1];
 
-    k = zeros(4,4)
-    BC = zeros(2,4)
+    k = zeros(4,4);
+    BC = zeros(2,4);
 
     for i=1:2
-        r = PG[1,i]
-        wx = w[1,i]
+        r = PG[1,i];
+        wx = w[1,i];
         for j = 1:2
-            s = PG[1,j]
-            wy = w[1,j]
+            s = PG[1,j];
+            wy = w[1,j];
             B,J = Q4BMatrix(r, s, x, y)
             dJ = det(J)
-            k  += B'*C*B*dJ*wx*wy
+            k  += B'*C*B*dJ*wx*wy;
             BC += C*B*dJ*wx*wy;
         end
     end
@@ -122,29 +144,29 @@ end
 
 # -----------------------------------------------------
 # Q4BMatrix - FEM
-function Q4BMatrix(r, s, x, y)
+function Q4BMatrix(r::Float64, s::Float64, x::Array{Float64,1}, y::Array{Float64,1})
 
-    X = [x'; y']
+    X = [x'; y'];
 
-    dN1dr = -(1-r)*.25
-    dN2dr =  (1-r)*.25
-    dN3dr =  (1+r)*.25
-    dN4dr = -(1+r)*.25
+    dN1dr = -(1-r)*.25;
+    dN2dr =  (1-r)*.25;
+    dN3dr =  (1+r)*.25;
+    dN4dr = -(1+r)*.25;
 
-    dN1ds = -(1-s)*.25
-    dN2ds = -(1+s)*.25
-    dN3ds =  (1+s)*.25
-    dN4ds =  (1-s)*.25
+    dN1ds = -(1-s)*.25;
+    dN2ds = -(1+s)*.25;
+    dN3ds =  (1+s)*.25;
+    dN4ds =  (1-s)*.25;
 
     dN = [dN1dr dN2dr dN3dr dN4dr;
-          dN1ds dN2ds dN3ds dN4ds]
+          dN1ds dN2ds dN3ds dN4ds];
 
-    J = dN*X'
+    J = dN*X';
 
-    dNdx = J\dN
+    dNdx = J\dN;
 
     B = [dNdx[1,1] dNdx[1,2] dNdx[1,3] dNdx[1,4];
-         dNdx[2,1] dNdx[2,2] dNdx[2,3] dNdx[2,4]]
+         dNdx[2,1] dNdx[2,2] dNdx[2,3] dNdx[2,4]];
 
 return B, J
 end
@@ -162,7 +184,7 @@ function dof_modes()
 println("----")
 println("Degree of Freedom Map")
 
-dofMap = zeros(Int64,m_ny+1,m_nx+1);
+dofMap = zeros(UInt64,m_ny+1,m_nx+1);
 
 dofMap[1:m_ny,1:m_nx] = reshape(1:m_numElem,m_ny,m_nx);
 dofMap[end,:] = dofMap[1,:];
@@ -176,12 +198,12 @@ end
 # -----------------------------------------------------
 # Direct Solver
 # [K] 32 or 64 bits * m_nGDL * m_nGDL double
-function directsolver(dofMap, RHS, RHS_2)
+function directsolver(dofMap::Array{UInt64,1}, RHS::Array{Float64,1}, RHS_2::Array{Float64,1})
 
     println("----")
     println("Direct Solver")
 
-    K = spzeros(m_nGDL,m_nGDL);
+    K = zeros(m_nGDL,m_nGDL);
     edof1 = 4; edof2 = 4;
     for e=1:m_numElem
         c = floor(UInt64, ((e-1)/m_ny) + 1);
@@ -201,8 +223,8 @@ function directsolver(dofMap, RHS, RHS_2)
 
     end
 
-    Tx = K.\RHS;
-    Ty = K.\RHS_2;
+    Tx = K\RHS;
+    Ty = K\RHS_2;
 
 return Tx, Ty
 end
@@ -212,11 +234,11 @@ end
 # Compute RHS
 # c_RHS Boundary = 0 || Domain = 1
 # axis x = 0 || y = 1
-function computeRHS(dofMap,c_RHS,axis)
+function computeRHS(dofMap::Array{UInt64,1}, c_RHS::Int64, axis::Int64)
 
     println("----")
     println("Compute RHS")
-    m_RHS = zeros(m_nGDL,1);
+    m_RHS = zeros(m_nGDL);
 
     if c_RHS == 0     # Boundary
 
@@ -226,16 +248,16 @@ function computeRHS(dofMap,c_RHS,axis)
             c = m_nx;
             for r=1:m_ny
                 elem = r + (c-1)*m_ny;
-                k = m_k[:,:,m_materials_I[m_matID[elem],1],1];
+                k = m_k[:,:,m_materials_I[m_matID[elem]]];
                 N1 = r + (c-1)*m_ny + c;
                 N2 = r + c*m_ny + c + 1;
                 N3 = r + c*m_ny + c;
                 N4 = r + (c-1)*m_ny + c - 1;
 
-                m_RHS[dofMap[N1],1] += - (k[1,2] + k[1,3])*deltaT;
-                m_RHS[dofMap[N2],1] += - (k[2,2] + k[2,3])*deltaT;
-                m_RHS[dofMap[N3],1] += - (k[3,2] + k[3,3])*deltaT;
-                m_RHS[dofMap[N4],1] += - (k[4,2] + k[4,3])*deltaT;
+                m_RHS[dofMap[N1]] += - (k[1,2] + k[1,3])*deltaT;
+                m_RHS[dofMap[N2]] += - (k[2,2] + k[2,3])*deltaT;
+                m_RHS[dofMap[N3]] += - (k[3,2] + k[3,3])*deltaT;
+                m_RHS[dofMap[N4]] += - (k[4,2] + k[4,3])*deltaT;
             end
 
         elseif axis == 1
@@ -244,16 +266,16 @@ function computeRHS(dofMap,c_RHS,axis)
             r = 1;
             for c=1:m_nx
                 elem = r + (c-1)*m_ny;
-                k = m_k[:,:,m_materials_I[m_matID[elem],1],1];
+                k = m_k[:,:,m_materials_I[m_matID[elem]]];
                 N1 = r + (c-1)*m_ny + c;
                 N2 = r + c*m_ny + c + 1;
                 N3 = r + c*m_ny + c;
                 N4 = r + (c-1)*m_ny + c - 1;
 
-                m_RHS[dofMap[N1],1] += - (k[1,3] + k[1,4])*deltaT;
-                m_RHS[dofMap[N2],1] += - (k[2,3] + k[2,4])*deltaT;
-                m_RHS[dofMap[N3],1] += - (k[3,3] + k[3,4])*deltaT;
-                m_RHS[dofMap[N4],1] += - (k[4,3] + k[4,4])*deltaT;
+                m_RHS[dofMap[N1]] += - (k[1,3] + k[1,4])*deltaT;
+                m_RHS[dofMap[N2]] += - (k[2,3] + k[2,4])*deltaT;
+                m_RHS[dofMap[N3]] += - (k[3,3] + k[3,4])*deltaT;
+                m_RHS[dofMap[N4]] += - (k[4,3] + k[4,4])*deltaT;
             end
 
         end
@@ -262,22 +284,22 @@ function computeRHS(dofMap,c_RHS,axis)
 
         for e=1:m_numElem
             c = floor(UInt64, ((e-1)/m_ny) + 1);
-            b = m_B[:,:,m_materials_I[m_matID[e],1],1];
+            b = m_B[:,:,m_materials_I[m_matID[e]]];
             N1 = e + c;
             N2 = e + c + 1 + m_ny;
             N3 = e + c + m_ny;
             N4 = e + c - 1;
 
             if axis == 0
-                m_RHS[m_dofMap[N1],1] += b[1,1];
-                m_RHS[m_dofMap[N2],1] += b[1,2];
-                m_RHS[m_dofMap[N3],1] += b[1,3];
-                m_RHS[m_dofMap[N4],1] += b[1,4];
+                m_RHS[m_dofMap[N1]] += b[1,1];
+                m_RHS[m_dofMap[N2]] += b[1,2];
+                m_RHS[m_dofMap[N3]] += b[1,3];
+                m_RHS[m_dofMap[N4]] += b[1,4];
             elseif axis == 1
-                m_RHS[m_dofMap[N1],1] += b[2,1];
-                m_RHS[m_dofMap[N2],1] += b[2,2];
-                m_RHS[m_dofMap[N3],1] += b[2,3];
-                m_RHS[m_dofMap[N4],1] += b[2,4];
+                m_RHS[m_dofMap[N1]] += b[2,1];
+                m_RHS[m_dofMap[N2]] += b[2,2];
+                m_RHS[m_dofMap[N3]] += b[2,3];
+                m_RHS[m_dofMap[N4]] += b[2,4];
             end
         end
 
@@ -289,15 +311,15 @@ end
 
 # -----------------------------------------------------
 # Pre Conjugate Gradient Solver
-function pcg(dofMap, r)
-
+function pcg(dofMap::Array{UInt64,1}, r::Array{Float64,1})
+   
     println("----")
     println("PCG Solver")
 
 # Preconditioner Jacobi assembly || M
 # -----------------------------------------------------
 
-    M = zeros(m_nGDL,1)
+    M = zeros(m_nGDL);
 
         for e=1:m_numElem
             c = floor(UInt64, ((e-1)/m_ny) + 1);
@@ -306,23 +328,23 @@ function pcg(dofMap, r)
             N2 = e + c + 1 + m_ny;
             N3 = e + c + m_ny;
             N4 = e + c - 1;
-            M[dofMap[N1],1] += k[1,1];
-            M[dofMap[N2],1] += k[2,2];
-            M[dofMap[N3],1] += k[3,3];
-            M[dofMap[N4],1] += k[4,4];
+            M[dofMap[N1]] += k[1,1];
+            M[dofMap[N2]] += k[2,2];
+            M[dofMap[N3]] += k[3,3];
+            M[dofMap[N4]] += k[4,4];
         end
 
 # -----------------------------------------------------
 # Compute r
 # -----------------------------------------------------
-    x = zeros(m_nGDL,1);
+    x = zeros(m_nGDL);
     edof1 = 4;
     edof2 = 4;
 
         # loop in elements to compute r
         for e=1:m_numElem
             c = floor(UInt64, ((e-1)/m_ny) + 1);
-            k = m_k[:,:,m_materials_I[m_matID[e],1],1];
+            k = m_k[:,:,m_materials_I[m_matID[e]]];
 
             N1 = e + c;
             N2 = e + c + 1 + m_ny;
@@ -353,11 +375,11 @@ function pcg(dofMap, r)
     # while (ii<i_max) && (abs(delta_new)>eps*eps*abs(delta_0))
     while (ii<=i_max) && (maximum(abs.(r))>m_tol)
 
-        q = zeros(m_nGDL,1);
+        q = zeros(m_nGDL);
 
         for e=1:m_numElem
             c = floor(UInt64, ((e-1)/m_ny) + 1);
-            k = m_k[:,:,m_materials_I[m_matID[e],1],1];
+            k = m_k[:,:,m_materials_I[m_matID[e]]];
 
             N1 = e + c;
             N2 = e + c + 1 + m_ny;
@@ -387,7 +409,7 @@ function pcg(dofMap, r)
         s = M.\r;
         delta_old = delta_new;
         delta_new = transpose(r)*s;
-        beta = delta_new./delta_old;
+        beta = delta_new/delta_old;
         d = s + d*beta;
         ii += 1;
 
@@ -403,21 +425,21 @@ end
 # -----------------------------------------------------
 # Recover Temperature Values
 # axis x = 0 || y = 1
-function rcvTemp( T1, c_RHS, axis)
+function rcvTemp( T1::Array{Float64,1}, c_RHS::Int, axis::Int)
 
 println("----")
 println("Recover Temperature Values")
 
-T0 = zeros(m_numNos,1);
+T0 = zeros(m_numNos);
 
 if c_RHS == 0    # Boundary
 
     if axis == 0
         deltaT = m_nx;
-        T0[(m_numNos-m_ny):m_numNos] .= deltaT;
+        T0[(m_numNos-m_ny):m_numNos] .= deltaT;  
     elseif axis == 1
         deltaT = m_ny;
-        T0[1:(m_ny+1):m_numNos] .= deltaT;
+        T0[1:(m_ny+1):m_numNos] .= deltaT;     
     end
     T = T1 + T0;
 
@@ -440,7 +462,7 @@ end
 # -----------------------------------------------------
 # Compute Flux - FEM
 # Effective property
-function femEffective(Tx,Ty)
+function femEffective(Tx::Array{Float64,1},Ty::Array{Float64,1})
 
 println("----")
 println("Compute Effective Property")
@@ -472,8 +494,9 @@ end
 # -----------------------------------------------------
 
 # -----------------------------------------------------
-
-
+function memestimate()
+    println
+end
 # -----------------------------------------------------
 
 # -----------------------------------------------------
@@ -485,37 +508,20 @@ end
 # ----------------
 # filename = ARGS[1] * ".nf"
 # m_tol , m_materials_I, m_materials_F, m_numMat = readNF(filename)
+@time begin
+    
 
-# ----------------
+# -----------------
 # Read File | JSON
-# ----------------
+# -----------------
 filename = ARGS[1] * ".json"
 m_tol , m_materials_I, m_materials_F, m_numMat, m_nx, m_ny = readJSON(filename)
 
 # ----------------
 # Read File | RAW
 # ----------------
-println("----")
-println("Read RAW")
-
 filename = ARGS[1] * ".raw";
-io = open(filename, "r");
-bin_array = read(io);
-
-if length(ARGS) == 1
-    m_nref = 1;
-else
-    m_nref = parse(UInt64,ARGS[2]);
-end
-
-m_matID = reshape(reinterpret(UInt8, bin_array),:);
-m_matID = reshape(m_matID,m_nx,m_ny);
-m_matID = convert(Array{UInt64},kron(m_matID,ones(m_nref,m_nref))).+1;
-m_matID = m_matID[:];
-
-m_nx = m_nx*m_nref; m_ny = m_nx;
-# Clear memory
-bin_array = nothing; io = nothing;
+m_matID , m_nx, m_ny = readRAW(filename,m_nx,m_ny)
 
 # ------------------
 # Initial Variables
@@ -544,29 +550,26 @@ end
 # ----------------------
 m_dofMap = dof_modes();
 
-# ----------------------------------
+# ---------------------------------
 # Compute RHS - Boundary or Domain
-# ----------------------------------
+# ---------------------------------
 # c_RHS Boundary = 0 || Domain = 1
-#[m_RHS]   % 32 or 64 bits * m_nGDL double  || axis 0 = X
-#[m_RHS_2] % 32 or 64 bits * m_nGDL double  || axis 1 = Y
+# axis 0 = X || axis 1 = Y
 c_RHS = 0;
 m_RHS   = computeRHS(m_dofMap, c_RHS, 0);
 m_RHS_2 = computeRHS(m_dofMap, c_RHS, 1);
 
-# ----------------
+# --------
 # SOLVERS
-# ----------------
+# --------
 # Direct Solver
-@time begin
-    tx, ty = directsolver(m_dofMap, m_RHS,m_RHS_2);
-end
+# tx, ty = directsolver(m_dofMap, m_RHS, m_RHS_2);
 
 # Preconditioned Conjugate Gradient Solver
-@time begin
-    tx1 = pcg(m_dofMap, m_RHS);
-end
-# tx = pcg(m_dofMap, m_RHS);
+# @time begin
+#     tx1 = pcg(m_dofMap, m_RHS);
+# end
+tx = pcg(m_dofMap, m_RHS);
 ty = pcg(m_dofMap, m_RHS_2);
 m_RHS = nothing; m_RHS_2 = nothing;
 # println(varinfo())
@@ -574,14 +577,15 @@ m_RHS = nothing; m_RHS_2 = nothing;
 # ----------------
 # Recover Values
 # ----------------
-# Tx 32 or 64 bits * m_nGDL double || Ty 64 bits * m_nGDL double
+# Tx 64 bits * m_nGDL double || Ty 64 bits * m_nGDL double
 Tx = rcvTemp(tx[m_dofMap], c_RHS, 0);
 Ty = rcvTemp(ty[m_dofMap], c_RHS, 1);
 tx = nothing; ty = nothing; m_dofMap = nothing;
-
 
 # ---------------------------
 # Compute Effective Property
 # ---------------------------
 C = femEffective(Tx,Ty);
 println(C)
+
+end
